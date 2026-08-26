da19878b755cb9dbd5c36af04c9cc03ec3ef1cb9
khaldwlydhmwdahmdalrhmany-ctrl <khaldwlydhmwdahmdalrhmany@gmail.com>
2026-08-27 00:58:02 +0300

    Refactor App component layout with Header and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 import HomePage from './pages/HomePage';
 import CategoryPage from './pages/CategoryPage';
 import ProductPage from './pages/ProductPage';
@@ -10,21 +13,41 @@ import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
+    <div className="min-h-screen flex flex-col" dir="rtl">
+      <Header />
 
-      <Route path="/filters" element={<CategoryPage categoryKey="filters" />} />
-      <Route path="/coolers" element={<CategoryPage categoryKey="coolers" />} />
-      <Route path="/mist" element={<CategoryPage categoryKey="mist" />} />
-      <Route path="/maintenance" element={<CategoryPage categoryKey="maintenance" />} />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
 
-      <Route path="/product/:productId" element={<ProductPage />} />
+          <Route
+            path="/filters"
+            element={<CategoryPage categoryKey="filters" />}
+          />
+          <Route
+            path="/coolers"
+            element={<CategoryPage categoryKey="coolers" />}
+          />
+          <Route
+            path="/mist"
+            element={<CategoryPage categoryKey="mist" />}
+          />
+          <Route
+            path="/maintenance"
+            element={<CategoryPage categoryKey="maintenance" />}
+          />
 
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/projects" element={<ProjectsPage />} />
-      <Route path="/quote" element={<QuotePage />} />
+          <Route path="/product/:productId" element={<ProductPage />} />
 
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/quote" element={<QuotePage />} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
   );
 }
