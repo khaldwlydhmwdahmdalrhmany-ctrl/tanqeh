893f2020a576e744f3de0dbebffe79a31f3f3f12
khaldwlydhmwdahmdalrhmany-ctrl <khaldwlydhmwdahmdalrhmany@gmail.com>
2026-08-29 17:08:16 +0300

    fix: restore shared layout routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Layout from './components/Layout';
 
 import HomePage from './pages/HomePage';
 import CategoryPage from './pages/CategoryPage';
@@ -13,41 +12,23 @@ import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col" dir="rtl">
-      <Header />
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
 
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
+        <Route path="/filters" element={<CategoryPage categoryKey="filters" />} />
+        <Route path="/coolers" element={<CategoryPage categoryKey="coolers" />} />
+        <Route path="/mist" element={<CategoryPage categoryKey="mist" />} />
+        <Route path="/maintenance" element={<CategoryPage categoryKey="maintenance" />} />
 
-          <Route
-            path="/filters"
-            element={<CategoryPage categoryKey="filters" />}
-          />
-          <Route
-            path="/coolers"
-            element={<CategoryPage categoryKey="coolers" />}
-          />
-          <Route
-            path="/mist"
-            element={<CategoryPage categoryKey="mist" />}
-          />
-          <Route
-            path="/maintenance"
-            element={<CategoryPage categoryKey="maintenance" />}
-          />
+        <Route path="/product/:productId" element={<ProductPage />} />
 
-          <Route path="/product/:productId" element={<ProductPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/quote" element={<QuotePage />} />
 
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/quote" element={<QuotePage />} />
-
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </main>
-
-      <Footer />
-    </div>
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 }
